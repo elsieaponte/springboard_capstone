9e0a1bece0fe668ae7a40a3cad828bb1aa9770ff
Elsie Aponte <elsie_aponte@outlook.com>
2025-07-21 09:55:50 -0400

    added jwtpayload

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -1,10 +1,15 @@
 import { NextRequest } from "next/server";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
-export const getDataFromToken = (request: NextRequest) => {
+export const getDataFromToken = (request: NextRequest): string => {
     try {
         const token = request.cookies.get("token")?.value || '';
-        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET!);
+        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET!) as string | JwtPayload;
+
+        if(typeof decodedToken === 'string'){
+            throw new Error('Invalid token payload: expected object but got string')
+        }
+
         return decodedToken.id;
     } catch (error: any) {
         throw new Error(error.message);
